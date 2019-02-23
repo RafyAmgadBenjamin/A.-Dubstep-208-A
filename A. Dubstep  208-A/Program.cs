b9bc85adbe7e131bef4163486be6d769e43fab4b
Rafy Amgad Benjamin <rafy.amgadbenjamin@gmail.com>
2019-02-23 20:46:27 +0200

    After getting accepted

--- A. Dubstep  208-A/Program.cs
+++ A. Dubstep  208-A/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace A.Dubstep__208_A
 {
@@ -10,6 +6,39 @@ namespace A.Dubstep__208_A
     {
         static void Main(string[] args)
         {
+
+            string song = Console.ReadLine();
+            Console.WriteLine(ReturnToOriginals(song));
+        }
+
+        public static string ReturnToOriginals(string song)
+        {
+            song += "WUB";
+            string songLyrics = "";
+            string tempWord = "";
+            for (int i = 0; i < song.Length - 2; i++)
+            {
+                if (song[i] == 'W' && song[i + 1] == 'U' && song[i + 2] == 'B')
+                {
+
+                    i = i + 2;//Jump a word(WUP)
+                    if (tempWord != "")
+                    {
+                        songLyrics += (tempWord + " ");
+                        tempWord = "";
+                    }
+                }
+                else if (i == song.Length - 2)
+                {
+
+                }
+                else
+                {
+                    tempWord += song[i];
+
+                }
+            }
+            return songLyrics.Trim();
         }
     }
 }
